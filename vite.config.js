fafeec543c13cd934597b2bd5dc307a3f8e99f48
KevinCll <malkev.malkavian@gmail.com>
2025-01-14 03:46:02 +0100

    t

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MEET_DRAGON_COJO/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
